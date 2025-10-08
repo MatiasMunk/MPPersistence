65fb8b022f3c53762304ce762f993eca8e27e9ed
Abdullahi <caduur0513@gmail.com>
2025-10-08 13:43:24 +0200

    Update GunReplica.java

--- src/model/GunReplica.java
+++ src/model/GunReplica.java
@@ -1,5 +1,33 @@
 package model;
 
 public class GunReplica {
-
+	private String caliber;
+	private String material;
+    
+	public GunReplica(String caliber, String material) {
+		this.caliber = caliber;
+		this.material = material;
+	}
+	
+	public String getCaliber() {
+		return caliber;
+	}
+	
+	public void setCaliber(String caliber) {
+		this.caliber = caliber;
+	}
+	
+	public String getMaterial() {
+		return material;
+	}
+	
+	public void setMaterial(String material) {
+		this.material = material;
+		
+	}
+	
+	public static GunReplica gunReplica(String caliber, String material) {
+		return new GunReplica(caliber, material);
+	}
+	
 }
