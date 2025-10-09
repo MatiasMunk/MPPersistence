b03cbf24a292b1c66d02b8cac5968b80ea76d8aa
Abdullahi <caduur0513@gmail.com>
2025-10-09 09:43:43 +0200

    Update Customer.java

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,16 +1,43 @@
 package model;
 
 public class Customer {
-    private String phoneNumber, name, address, zipcode, city, type;
-
-    public Customer(String phoneNumber, String name, String address, String zipcode, String type) {
-        this.name = name;
-        this.address = address;
-        this.zipcode = zipcode;
-        this.phoneNumber = phoneNumber;
-        this.type = type;
-    }
-
-    public void setName(String name) { this.name = name; }
-    public String getName() { return name; }
-}
+	private String name;
+	private String phoneNo;
+	private String type;
+	
+	public Customer(String name, String address, String zipcode, String city, String phoneNumber, String type) {
+		this.name = name;
+		this.phoneNo = phoneNumber; 
+		this.type = type;
+	}
+		
+	
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public String getPhoneno() {
+			return phoneNo;
+		}
+		
+		public void setPhoneno(String phoneno) {
+			this.phoneNo = phoneno;
+		}
+		
+		public String getType() {
+			return type;
+		}
+		
+		public void setType(String type) {
+			this.type = type;
+		}
+		
+		public static Customer customer(String name, String address, String zipcode, String city, String phoneNumber, String type) {
+			return new Customer(name, address, zipcode, city, phoneNumber, type);
+		} 
+	}
+   
