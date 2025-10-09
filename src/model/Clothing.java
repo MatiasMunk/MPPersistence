6df7101d848254a39e5568acbfa6e13865fc4759
Abdullahi <caduur0513@gmail.com>
2025-10-09 09:46:28 +0200

    Update Clothing.java

--- src/model/Clothing.java
+++ src/model/Clothing.java
@@ -1,5 +1,33 @@
 package model;
 
 public class Clothing {
+	private String size;
+	private String colour;
+	
+	
+	public Clothing(int productNumber, String name, int minStock, String format, String artist, String colour, String size) {
+		this.size = size;
+		this.colour = colour;
+	}
+	
+	public String getSize() {
+		return size;
+	}
+	
+	public void setSize(String size) {
+		this.size = size;
+	}
+	
+	public String getColour() {
+		return colour;
+	}
+	
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	
+    public static Clothing clothing(int productNumber, String name, int minStock, String format, String artist, String colour, String size) {
+		return new Clothing(productNumber, name, minStock, format, artist, colour, size);
+	}
 
 }
